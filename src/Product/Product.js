817f58a69de9bb91e3fca97d2fb702b64ca20f88
Mamuni98 <nabanitasahoo468@gmail.com>
2024-05-03 20:53:06 +0530

    productDetailPage_errorHandle

--- src/Product/Product.js
+++ src/Product/Product.js
@@ -13,23 +13,30 @@ const Product = ({ id, title, price, catagory, description }) => {
     dispatch(productActions.editProduct(id));
     navigate("/addProduct");
   };
-  const productDetailHandler = ()=>{
-    dispatch(productActions.addProductDetail({id:id, title:title, price:price, catagory:catagory, description:description}))
-  }
+  const productDetailHandler = () => {
+    dispatch(
+      productActions.addProductDetail({
+        id: id,
+        title: title,
+        price: price,
+        catagory: catagory,
+        description: description,
+      })
+    );
+  };
   return (
-    <Link to={`/product/${id}`} style={{textDecoration:"none"}}>
-    <li className={classes.list} onClick={productDetailHandler}>
-      <div className={classes.listcontent}>
-        <h3>{title}</h3>
-        <h4>Rs. {price}</h4>
-      </div>
-
+    <li className={classes.list}>
+      <Link to={`/product/${id}`} style={{ textDecoration: "none" }}>
+        <div className={classes.listcontent} onClick={productDetailHandler}>
+          <h3>{title}</h3>
+          <h4>Rs. {price}</h4>
+        </div>
+      </Link>
       <div className={classes.listbtns}>
         <button onClick={editProductHandler}>Edit</button>
         <button onClick={deleteProductHandler}>Delete</button>
       </div>
     </li>
-    </Link>
   );
 };
 
